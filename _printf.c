f9557ecef50a1a5ead6d465b52f1e5cb57cbe465
driss-chnc <chaneac.driss@gmail.com>
2026-07-23 15:02:56 +0200

    mod _printf.c

--- _printf.c
+++ _printf.c
@@ -7,4 +7,37 @@
  */
 int _printf(const char *format, ...)
 {
+    va_list args;
+    int i;
+    int count;
+
+    if (format == NULL)
+    {                       //verifie directly if the pointer format is null and return -1 error if true
+        return (-1);
+    }
+
+    va_start(args, format);
+    i = 0;
+    count = 0;
+
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+        {
+            count += _pucthar(format[i]);
+            i++;
+        }
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+            {
+                count += print_char(args);
+            }
+            i++;
+        }
+    }
+
+    va_end(args);
+    return (count);
 }
